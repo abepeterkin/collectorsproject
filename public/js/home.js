3f0eaa12f904d1943058a7245f53725eed86176e
Kelly Wang <kelly_wang@brown.edu>
2017-04-27 23:48:41 -0400

    changed modal boxes

--- public/js/home.js
+++ public/js/home.js
@@ -1,78 +1,98 @@
-$(document).ready(function(){
+window.onload = function() {
+	var numImages = 2;
+    var randomNumber = Math.floor(Math.random() * numImages) + 1;
+	$("#backgroundImageID").css("background-image", "url(images/background_" + randomNumber + ".jpg");
+}
 
-var modal = false;
-var results = false;
-
-background();
-checkUser();
 
 var searchbar = $("#search");
 
-if (username != "") {
-	$("#profile").html(username);
-	$("#signIn").hide();
-	$("#signUp").hide();
-}
 
-	if (modal == false) {
-		/************************************/
-		/*			Signup button			*/
-		/************************************/
-		$("#signUp").click(function(){
-			modal = true;
-			$.ajax({
-				type: "GET",
-				url: "/signup",
-				success: function(data){
-					$("#modalBox").html(data);
-					$("#modal").fadeIn();
-				}
-			});
-		});
+$(document).ready(function(){
 
-		/************************************/
-		/*	Redirect to signup from login	*/
-		/************************************/
-		$("#signIn").click(function() {
-			modal = true;
-			$.ajax({
-				type: "GET",
-				url: "/signin",
-				success: function(data){
-					$("#modalBox").html(data);
-					$("#modal").fadeIn();
-				}
-			});
-		});
+	console.log("USER: " + username);
 
-		/************************************/
-		/*				Upload 				*/
-		/************************************/
-			$("#upload").click(function() {
-				modal = true;
-				$.post("upload", function(data){
-					console.log(data);
-				});
-			});
+	$("#tester").html(username);
+
+	var span = document.getElementsByClassName("close")[0];
+
+	if (username === "") {
+		$("#username").hide();
+		$("#profile").hide();
+		$("#signOut").hide();
+		$("#upload").hide();
+	} else {
+		$("#profile").html(username);
+		$("#signIn").hide();
+		$("#signUp").hide();
+	}
+
+	$("#signUp").click(function() {
+	    $("#signUpModal")[0].style.display = "flex";
+		$("#signUpModal")[0].style.backgroundColor = "hsla(0,0%,0%,0.5)";
+	});
+
+	$("#signIn").click(function() {
+	    $("#signInModal")[0].style.display = "flex";
+		$("#signInModal")[0].style.backgroundColor = "hsla(0,0%,0%,0.5)";
+	});
+
+	$("#upload").click(function() {
+	    $("#uploadModal")[0].style.display = "flex";
+		$("#uploadModal")[0].style.backgroundColor = "hsla(0,0%,0%,0.5)";
+	});
 
 
-		/************************************/
-		/*			Profile Modal			*/
-		/************************************/
-			$("#profile").click(function() {
-				modal = true;
-				$.post("profile", function(data){
-					$("#modal").html(data).fadeIn();
-				});
-			});
+	$(document).click(function(event) {
+    	if (event.target == $("#signUpModal")[0]) {
+			$("#signUpModal")[0].style.display = "none";
+		}
+	});
 
-		/************************************/
-		/*			Search Modal			*/
-		/************************************/
-			searchbar.keyup(function(event){
-				$("#search_results").html("");
-				if (searchbar.val() !== "") {
-					$.post("search/" + searchbar.val(), function(result){
+	$(document).click(function(event) {
+    	if (event.target == $("#signInModal")[0]) {
+			$("#signInModal")[0].style.display = "none";
+		}
+	});
+
+	$(document).click(function(event) {
+    	if (event.target == $("#uploadModal")[0]) {
+			$("#uploadModal")[0].style.display = "none";
+		}
+	});
+
+
+
+	$("#search").keyup(function(event){
+
+			//document.getElementById("body_words").remove();
+			$("#top").hide();
+	    	document.getElementById("body_text").display="block";
+		    document.getElementById("body_text").style.top="75px"; 
+
+		    document.getElementById("header").style.height = "50px";
+
+
+		    document.getElementById("nav_bar_title").style.fontSize = "20px";
+		    document.getElementById("news").style.fontSize = "15px";
+		    document.getElementById("about").style.fontSize = "15px";
+		    document.getElementById("signIn").style.fontSize = "15px";
+		    document.getElementById("signUp").style.fontSize = "15px";
+
+		    document.getElementById("upload").style.fontSize = "15px";
+		    document.getElementById("profile").style.fontSize = "15px";
+		    document.getElementById("signOut").style.fontSize = "15px";
+
+		    document.getElementById("search_results").style.visibility="visible";
+		    document.getElementById("search_results").style.top="20px";
+		    document.getElementById("search_results").style.height="600px";
+
+
+		$("#search_results").html("");
+				if ($("#search").val() !== "") {
+
+				    console.log($("#search").val());
+					$.post("search/" + $("#search").val(), function(result){
 						var resultObjects = JSON.parse(result);
 						for (var key in resultObjects) {
 							if (resultObjects.hasOwnProperty(key)) {
@@ -84,49 +104,6 @@ if (username != "") {
 						}
 					});
 				}
-				/*modal = true;
-				results = true;
-				if(event.keyCode == 13){
-					query = $("#search").val();
-					$("#body_words").hide();
-					$("#body_text").display = "block";
-					console.log($("#body_text"));
-//					$("#body_text").style.top = "75px";
-//					$("#header").style.height = "50px";
-					$.post("search/" + query, function(data){
-						$("#modal").html(data).fadeIn();
-					});
-				}*/
-			});
+	});
 
-		/************************************/
-		/*			Exit modal boxes		*/
-		/************************************/
-			$(document).click(function(event) {
-				if (event.target == $("#modal")[0]) {
-					if (results == true) {
-						$("#body_words").show();
-					}
-					$("#modal").fadeOut();
-					modal = false;
-					console.log(modal);
-				}
-			});
-		console.log(modal);
-	}
-
-	function checkUser() {
-		if (username === "") {
-			$("#username").hide();
-			$("#profile").hide();
-			$("#signOut").hide();
-			$("#upload").hide();
-		}
-	}
-
-	function background() {
-		var numImages = 2;
-		var randomNumber = Math.floor(Math.random() * numImages) + 1;
-		$("#backgroundImageID").css("background-image", "url(images/background_" + randomNumber + ".jpg");
-	}
 });
