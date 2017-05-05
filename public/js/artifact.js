c20904a6cdfbec8b3ae97935bfcf9474916a7cec
muildrik <martin_uildriks@brown.edu>
2017-05-05 00:57:00 -0400

    Experimental markup tool in artifact info
    This is experimental, but may perhaps work with what Colm had initially
    set up in the database structure in the backend

---
+++ public/js/artifact.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+var text = "";
+
+highlight();
+
+function highlight() {
+	// This function should retrieve all people, places, and time values from the database and highlight them in the provenance lists
+	$.get("/mark", function() {
+//		mark();
+	});
+}
+	
+function getSelectionText() {
+	if (window.getSelection) {
+		text = window.getSelection().toString();
+		mark(window.getSelection(), $("#artifact_provenance"));
+	} else if (document.selection && document.selection.type != "Control") {
+		text = document.selection.createRange().text;
+	}
+}
+
+function mark(word, element) {
+    var rgxp = new RegExp(word, 'g');
+    var repl = '<span class="highlighted">' + word + '</span>';
+	element[0].innerHTML = element[0].innerHTML.replace(rgxp, repl);
+	div = "#" + text;
+	$('.highlighted').contextmenu(function (event){
+		event.preventDefault();
+		var parentOffset = $(this).parent().offset();
+		var relX = event.pageX - parentOffset.left;
+		var relY = event.pageY - parentOffset.tsop;
+		$(".custom-menu").finish().toggle(100).
+		css({
+			top: relY + "px",
+			left: relX + "px"
+	    });
+	});
+};
+
+$(document).bind("mousedown", function (e) {
+	if (!$(e.target).parents(".custom-menu").length > 0) {
+		$(".custom-menu").hide(100);
+    }
+});
+
+$(".custom-menu li").click(function(){
+	switch($(this).attr("data-action")) {
+		case "first": 
+			// Add word to person database; update search results
+			break;
+		case "second": 
+			// Add word to time database; update search results
+			break;
+		case "third": 
+			// Add word to place database; update search results
+			break;
+    }
+	$(".custom-menu").hide(100);
+});
+
+});
